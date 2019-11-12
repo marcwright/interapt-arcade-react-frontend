8a81ada11d4acd0b29a7d25d58eb3da9d33e9476
Marc Wright <marc.wright@gmail.com>
2019-11-12 09:45:04 -0500

    Updates

---
+++ precache-manifest.f8fe210ab9b07e637a69073e75707363.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "089cbd99bf65acc32be9578bb81951be",
+    "url": "/marcwright-rem/interapt-arcade-react-frontend-deploy/index.html"
+  },
+  {
+    "revision": "1c56b9ef918d6da6ad8f",
+    "url": "/marcwright-rem/interapt-arcade-react-frontend-deploy/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d18763196fa8270b8f9c",
+    "url": "/marcwright-rem/interapt-arcade-react-frontend-deploy/static/js/2.160b39ff.chunk.js"
+  },
+  {
+    "revision": "1c56b9ef918d6da6ad8f",
+    "url": "/marcwright-rem/interapt-arcade-react-frontend-deploy/static/js/main.4860144f.chunk.js"
+  },
+  {
+    "revision": "99f6cd37bd4c577479b5",
+    "url": "/marcwright-rem/interapt-arcade-react-frontend-deploy/static/js/runtime-main.4164a6e8.js"
+  }
+]);
