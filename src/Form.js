80614ce15083b13bad14aedd03c7740e68e25a5d
Marc Wright <marc.wright@gmail.com>
2019-11-12 20:38:14 -0500

    Add push history

--- src/Form.js
+++ src/Form.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import {
+  withRouter
+} from 'react-router-dom'
 
 
 class Form extends React.Component {
@@ -15,8 +18,9 @@ class Form extends React.Component {
     this.textInput = React.createRef();
   }
 
-  onFormSubmit = (e) => {
+  onFormSubmit = e => {
     this.props.onFormSubmit(e, this.state)
+    this.props.history.push('/')
   }
 
   handleChange = e => {
@@ -28,38 +32,33 @@ class Form extends React.Component {
   }
 
   render() {
-    console.log(this.state)
+    console.log(this.props)
     return (
       <div className='col-md-6 offset-md-3'>
         <form className='new-form' onChange={e => this.handleChange(e)} onSubmit={e => this.onFormSubmit(e)}>
           <div className="form-group">
-            <label>Full Name:</label>
-            <input type="text" className="form-control" ref={this.textInput} name="fullName" />
+            <input type="text" placeholder="Full Name" className="form-control" ref={this.textInput} name="fullName" />
           </div>
           <div className="form-group">
-            <label>App Name:</label>
-            <input type="text" className="form-control" ref={this.textInput} name="appName" />
+            <input type="text" placeholder="App Name" className="form-control" ref={this.textInput} name="appName" />
           </div>
           <div className="form-group">
-            <label>Deployed URL:</label>
-            <input type="text" className="form-control" ref={this.textInput} name="deployUrl" />
+            <input type="text" placeholder="Deployed URL Link" className="form-control" ref={this.textInput} name="deployUrl" />
           </div>
           <div className="form-group">
-            <label>GitHub Repo:</label>
-            <input type="text" className="form-control" ref={this.textInput} name="gitHubRepo" />
+            <input type="text" placeholder="GitHub Repo URL" className="form-control" ref={this.textInput} name="gitHubRepo" />
           </div>
           <div className="form-group">
-            <label>Project:</label>
-            <select name="project">
+            <select className="custom-select" name="project">
               <option value="1">Project 1</option>
               <option value="2">Project 2</option>
               <option value="3">Project 3</option>
               <option value="4">Project 4</option>
             </select>
           </div>
-          <div className="form-group">
-            <label>Upload Screenshot:</label>
-            <input type="file" className="form-control" name="myImage" />
+          <div className="form-group custom-file">
+            <label className="custom-file-label">Upload Screenshot:</label>
+            <input type="file" className="custom-file-input form-control" name="myImage" />
           </div>
           <button type="submit" className="btn btn-success">Submit</button>
         </form>
@@ -68,4 +67,4 @@ class Form extends React.Component {
   }
 }
 
-export default Form
+export default withRouter(Form)
