37e57ef5299dd71356ea1b9992d43f026543a6f6
Marc Wright <marc.wright@gmail.com>
2019-11-12 20:13:48 -0500

    Claning  up form

--- src/App.js
+++ src/App.js
@@ -114,6 +114,7 @@ class App extends React.Component {
               <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                   <li className="nav-item active">
+                    <Link to='/newproject'>New Project</Link>
                   </li>
                 </ul>
                 <SearchBar
--- src/Form.js
+++ src/Form.js
@@ -30,38 +30,40 @@ class Form extends React.Component {
   render() {
     console.log(this.state)
     return (
-      <form className='new-form' onChange={e => this.handleChange(e)} onSubmit={e => this.onFormSubmit(e)}>
-        <div className="form-group">
-          <label>Full Name:</label>
-          <input type="text" className="form-control" ref={this.textInput} name="fullName" />
-        </div>
-        <div className="form-group">
-          <label>App Name:</label>
-          <input type="text" className="form-control" ref={this.textInput} name="appName" />
-        </div>
-        <div className="form-group">
-          <label>Deployed URL:</label>
-          <input type="text" className="form-control" ref={this.textInput} name="deployUrl" />
-        </div>
-        <div className="form-group">
-          <label>GitHub Repo:</label>
-          <input type="text" className="form-control" ref={this.textInput} name="gitHubRepo" />
-        </div>
-        <div className="form-group">
-          <label>Project:</label>
-          <select name="project">
-            <option value="1">Project 1</option>
-            <option value="2">Project 2</option>
-            <option value="3">Project 3</option>
-            <option value="4">Project 4</option>
-          </select>
-        </div>
-        <div className="form-group">
-          <label>Upload Screenshot:</label>
-          <input type="file" className="form-control" name="myImage" />
-        </div>
-        <button type="submit" className="btn btn-success">Submit</button>
-      </form>
+      <div className='col-md-6 offset-md-3'>
+        <form className='new-form' onChange={e => this.handleChange(e)} onSubmit={e => this.onFormSubmit(e)}>
+          <div className="form-group">
+            <label>Full Name:</label>
+            <input type="text" className="form-control" ref={this.textInput} name="fullName" />
+          </div>
+          <div className="form-group">
+            <label>App Name:</label>
+            <input type="text" className="form-control" ref={this.textInput} name="appName" />
+          </div>
+          <div className="form-group">
+            <label>Deployed URL:</label>
+            <input type="text" className="form-control" ref={this.textInput} name="deployUrl" />
+          </div>
+          <div className="form-group">
+            <label>GitHub Repo:</label>
+            <input type="text" className="form-control" ref={this.textInput} name="gitHubRepo" />
+          </div>
+          <div className="form-group">
+            <label>Project:</label>
+            <select name="project">
+              <option value="1">Project 1</option>
+              <option value="2">Project 2</option>
+              <option value="3">Project 3</option>
+              <option value="4">Project 4</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Upload Screenshot:</label>
+            <input type="file" className="form-control" name="myImage" />
+          </div>
+          <button type="submit" className="btn btn-success">Submit</button>
+        </form>
+      </div>
     )
   }
 }
